42914d7e85e80317e644fc7289c425774fd8c734
Guilherme Vieira <gui.vieira.code@gmail.com>
2023-05-24 10:08:25 -0300

    Use http requests to read, update and delete data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { CourseListComponent } from './course-list/course-list.component';
 import { StarComponent } from './star/star.component';
 import { ReplacePipe } from './pipes/replace.pipe';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
-import { RouterModule } from '@angular/router';
 import { ErrorComponent } from './error/error.component';
 import { CourseInfoComponent } from './course-info/course-info.component';
 
@@ -23,6 +24,7 @@ import { CourseInfoComponent } from './course-info/course-info.component';
   imports: [
     BrowserModule,
     FormsModule,
+    HttpClientModule,
     RouterModule.forRoot([
       {
         path: 'courses', component: CourseListComponent
